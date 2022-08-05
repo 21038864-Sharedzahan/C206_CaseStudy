28439438682573141b70783e356666f97fb0287f
20041279 <20041279@10.174.115.95>
2022-08-05 15:57:37 +0800

    Task 1

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,61 @@
+import java.util.ArrayList;
+import java.util.Currency;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<CurrencyCentre> CurrencyList = new ArrayList<CurrencyCentre>();
+
+		CurrencyList.add(new CurrencyCentre("Singapore Dollar","SIN","SGP",0.00 ,0.00, "05-Aug-2022", 0));
+		int option = 0;
+
+		while (option != 4) {
+
+			CurrencyCentre.menu();
+			option = Helper.readInt("Enter an option > ");
+
+		}  if (option == 1) {
+			// View all currency
+			CurrencyCentre.viewAllCurrency(CurrencyList);
+
+
+		} else if (option == 2) {
+			// Add item
+			CurrencyCentre.setHeader("ADD CURRENCY");
+
+			String addCurrency = Helper.readString("Add currency > ");
+
+			addCurrency.add(CurrencyList);
+
+
+
+		} else if (option == 3) {
+			// Delete currency
+			CurrencyCentre.setHeader("DELETE CURRENCY");
+
+			String delCurrency = Helper.readString("Delete currency by country> ");
+			delCurrency.remove(CurrencyList);
+		}else 
+			System.out.println("Bye!");
+	}
+
+	public static void menu() {
+		CurrencyCentre.setHeader("CURRENCY EXCHANGE APP");
+		System.out.println("1. Add currency");
+		System.out.println("2. View currency");
+		System.out.println("3. Delete currency");
+		System.out.println("4. Quit");
+		Helper.line(80, "-");
 
 	}
 
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
 }
+
+
+
---
+++ C206_CaseStudy/src/CurrencyCentre.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class CurrencyCentre {
+
+	private String currencyCountry; 
+	private String currencyName; 
+	private String isoCode;
+	private double buyRate;
+	private double sellRate;
+	private String date;
+	private double amount;
+	
+	public CurrencyCentre(String currencyCountry, String currencyName, String isoCode, double buyRate, double sellRate,
+			String date, double amount) {
+		super();
+		this.currencyCountry = currencyCountry;
+		this.currencyName = currencyName;
+		this.isoCode = isoCode;
+		this.buyRate = buyRate;
+		this.sellRate = sellRate;
+		this.date = date;
+		this.amount = amount;
+	}
+	
+	public double getBuyRate() {
+		return buyRate;
+	}
+	public void setBuyRate(double buyRate) {
+		this.buyRate = buyRate;
+	}
+	public double getSellRate() {
+		return sellRate;
+	}
+	public void setSellRate(double sellRate) {
+		this.sellRate = sellRate;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getCurrencyCountry() {
+		return currencyCountry;
+	}
+	public String getCurrencyName() {
+		return currencyName;
+	}
+	public String getIsoCode() {
+		return isoCode;
+	}
+
+	public static void menu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void viewAllCurrency(ArrayList<CurrencyCentre> currencyList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void setHeader(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addCurrencyCentre(ArrayList<CurrencyCentre> currencyList, Currency cc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+		
+}
+	
+	
